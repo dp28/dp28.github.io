a4032372e0a1f7e313a5a065ab8b25982ecc712e
Daniel Patterson <dan24patt@gmail.com>
2022-06-03 15:57:26 -0400

    Fix warning

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,27 @@
-import postcss from 'rollup-plugin-postcss';
-import replace from '@rollup/plugin-replace';
-import svg from 'rollup-plugin-svg';
-import { terser } from 'rollup-plugin-terser';
-import path from 'path';
+import postcss from "rollup-plugin-postcss";
+import replace from "@rollup/plugin-replace";
+import svg from "rollup-plugin-svg";
+import { terser } from "rollup-plugin-terser";
+import path from "path";
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 
 export default {
-  input: 'src/scripts/main.js',
+  input: "src/scripts/main.js",
   output: {
     sourcemap: false,
-    format: 'iife',
-    name: 'main',
-    file: 'dist/assets/main.bundle.js',
+    format: "iife",
+    name: "main",
+    file: "dist/assets/main.bundle.js",
   },
   plugins: [
     replace({
       DEV_MODE: dev,
+      preventAssignment: true,
     }),
     svg(),
     postcss({
-      extract: path.resolve('dist/assets/main.bundle.css'),
+      extract: path.resolve("dist/assets/main.bundle.css"),
       minimize: !dev,
     }),
     !dev && terser(),
